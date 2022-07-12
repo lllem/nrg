5a1af5b6a36932f58f3e90e1b89419d33fc189e8
lllem <s.m.shemiakin@gmail.com>
2022-07-12 21:06:36 +0300

    Navigation added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@
 
     <div class="container py-5">
       <h1>Home page</h1>
+
+      <p><a href="https://github.com/lllem/nrg">Проект на GitHub</a></p>
     </div>
 
     <pre
     v-highlightjs
     class="container py-5 overflow-hidden"
-    ><code class="json">{{ $router }}</code></pre>
+    ><code class="json">{{ $router.options.routes }}</code></pre>
 
   </div>
 </template>
