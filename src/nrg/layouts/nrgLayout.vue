8869db92c8c25031bed18d990eb46e4f296f3af7
lllem <s.m.shemiakin@gmail.com>
2022-07-12 20:06:29 +0300

    Layout component added

---
+++ src/nrg/layouts/nrgLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="nrg-layout bg-light">
+
+    <aside
+    class="nrg-layout__sidebar"
+    >
+      <slot name="sidebar"/>
+    </aside>
+
+    <main class="nrg-layout__body bg-white shadow">
+      <header class="bg-white shadow">
+        <slot name="header"/>
+      </header>
+
+      <section class="nrg-layout__banner px-3 py-5 bg-dark text-white text-center">
+        <slot name="banner"/>
+      </section>
+
+      <div class="nrg-layout__content">
+        <slot name="default"/>
+      </div>
+
+      <footer class="bg-dark text-white">
+        <slot name="footer"/>
+      </footer>
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+.nrg-layout {
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+  min-height: 100vh;
+
+  .nrg-layout__body {
+    width: 100%;
+    display: flex;
+    flex-grow: 1;
+    flex-flow: column nowrap;
+  }
+
+  .nrg-layout__banner {
+    // ...
+  }
+
+  .nrg-layout__content {
+    width: 100%;
+    flex-grow: 1;
+  }
+
+  .nrg-layout__sidebar {
+    width: 20rem; // TODO
+    max-width: 100%;
+    overflow-y: auto;
+  }
+}
+</style>
