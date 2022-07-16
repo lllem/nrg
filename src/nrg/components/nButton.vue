e2f19366ff818ad100d9e099c0d7eb9e6b1336d7
lllem <s.m.shemiakin@gmail.com>
2022-07-17 01:50:28 +0300

    Button component added

---
+++ src/nrg/components/nButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button>button</button>
+</template>
+
+<script>
+export default {
+  name: 'n-button',
+  props: {
+    to: [String, Object],
+    href: String,
+    size: {
+      type: String,
+      default: 'm',
+      validator: (value) => (value === '' || value.match(/(xs|s|m|l|xl)/)),
+    },
+    type: {
+      type: String,
+      default: 'contained',
+      validator: (value) => (value === '' || value.match(/(contained|outlined|text)/)),
+    },
+    iconic: Boolean,
+    shadow: Boolean,
+    reverse: Boolean,
+    justify: Boolean,
+    pulse: Boolean,
+    dense: Boolean,
+    disable: Boolean,
+    state: {
+      type: String,
+      default: '',
+      validator: (value) => (value === '' || value.match(/(loading|error|success)/)),
+    },
+  },
+};
+</script>
