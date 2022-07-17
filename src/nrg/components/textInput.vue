b7aa33d756c6a194126acc7cdf6edf6af128b8e4
lllem <s.m.shemiakin@gmail.com>
2022-07-17 05:56:04 +0300

    Improved input component

--- src/nrg/components/textInput.vue
+++ src/nrg/components/textInput.vue
@@ -2,26 +2,34 @@
 <!-- eslint-disable vuejs-accessibility/label-has-for -->
   <div
   class="nrg-input"
-  :class="{focus: inFocus}"
+  :class="{
+    'focus': inFocus,
+    'not-empty': notEmpty,
+  }"
   >
     <input
     type="text"
+    ref="input"
+    :value="modelValue"
     :id="id"
+    :placeholder="inFocus? placeholder : ''"
     class="nrg-input__intput"
     @focus="onFocus"
     @blur="onBlur"
     @change="onChange"
+    @input="onChange"
     />
     <label
+    v-if="label"
     :for="id"
     class="nrg-input__label"
     >
-      {{ 'label' }}
+      {{ label }}
     </label>
 
     <span class="nrg-input__indicator"></span>
 
-    <span class="nrg-input__helper">helper</span>
+    <span class="nrg-input__helper">{{ helper }}</span>
   </div>
 
 </template>
@@ -32,33 +40,57 @@ export default {
 
   setup() {},
 
+  model: {},
+
   props: {
     modelValue: String,
+    value: String,
+    helper: String,
+    label: String,
+    placeholder: String,
   },
 
   data() {
     return {
-      id: `input-text_${Math.random()*1000000}`,
+      id: `input-text_${Math.random() * 1000000}`,
       inFocus: false,
+      notEmpty: false,
     };
   },
 
+  computed: {},
+
   methods: {
-    onFocus(e) {
+    onFocus() {
       this.inFocus = true;
+      console.log('onFocus');
     },
-    onBlur(e) {
+    onBlur() {
+      this.inputCheck();
       this.inFocus = false;
+      console.log('onBlur');
     },
-    onChange(e) {
-      // ...
+    onChange(event) {
+      // this.$emit(event.type);
+      this.emit(event);
+      console.log('onChange');
     },
     emit(event) {
-      this.$emit(event.target.type);
-    }
+      console.log('emit');
+      this.inputCheck();
+      this.$emit('update:modelValue', event.target.value);
+      console.log(event.target.value);
+    },
+    inputCheck() {
+      this.notEmpty = Boolean(this.$refs.input.value);
+    },
   },
 
   created() {},
+
+  mounted() {
+    this.inputCheck();
+  },
 };
 </script>
 
@@ -70,7 +102,6 @@ export default {
   .nrg-input {
     position: relative;
     height: $ctrl_height;
-    border: 1px solid #ff000050;
 
     .nrg-input__intput {
       height: $ctrl_height;
@@ -86,24 +117,74 @@ export default {
       top: 0;
       line-height: $ctrl_height;
       pointer-events: none;
+      transform-origin: top left;
+      transition: 0.25s;
+      color: blue;
+      font-weight: 600;
     }
+
+    // helper
 
     .nrg-input__helper {
       position: absolute;
       left: 0;
-      top: $height;
+      top: $ctrl_height;
       font-size: 75%;
-      padding-top: 0.5rem;
+
+      text-overflow: ellipsis;
+      max-width: 100%;
+      overflow: hidden;
+      white-space: nowrap;
     }
+
+    // _________________
 
     .nrg-input__indicator {
       height: 2px;
       display: inline-block;
-      background-color: grey; // 2DO
+      background-color: blue; // 2DO
       position: absolute;
       bottom: 0;
       left: 0;
       width: 100%;
     }
+
+    &.focus,
+    &.not-empty {
+      .nrg-input__label {
+        transform: translateY(-50%) scale(80%);
+      }
+    }
   }
 </style>
+
+<!--
+  eslint-disable max-len
+
+  TODO
+
+  helper ___________________
+
+  - Может быть не только текстом, но и содержать ссылки. Как, например, предложение заполнить поле значением по-умолчанию
+
+  - Обычно в одну строку, то что не поместилось - обрезается, но в таком случае нужно реализовать возможность прочитать его полностью: title для десктопов, поповер для тач-девайсов, возможно сделать бегущей строкой когда поле в фокусе
+
+  - Комментарий может поменяться на сообщение об ошибке если поле невалидно
+
+  ___________________
+
+  - Учесть безопасные зоны для label и helper
+
+  - Сделать placeholder не нативным, а кастомным и анимировать
+
+  - Добавить валидацию и маски
+
+  - Добавить иконки
+
+  - Типы
+
+  - Добавить счётчик количества знаков при ограничении количества символов
+
+  - Добавить состояние когда идёт обмен данными с сервером
+
+ -->
